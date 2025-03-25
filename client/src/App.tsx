2c98b9a044dcc055d51b7bc4a6313688b5bbd9a3
nkamalu620 <nkamalu96@gmail.com>
2025-03-25 12:11:47 -0400

    removed router from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,14 @@
 import Navbar from './components/Navbar';
-import Login from './pages/Login';
-import KanbanBoard from './pages/Board';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/kanban' element={<KanbanBoard />} />
-      </Routes>
-    </Router>
+     <Outlet />
+    </>
   );
 };
 
